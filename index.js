be948f9afe8ea6d4b56c5348dad17e149360a2f1
Aditya-0806 <adityakulkarni17893@gmail.com>
2025-09-17 00:46:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+var whatsclicked=document.querySelectorAll(".drum");
+for (var i=0;i<document.querySelectorAll(".drum").length;i++){
+whatsclicked[i].addEventListener("click",function(){
+
+var exactly=this.innerHTML;
+switchstatment(exactly);
+Addanimation(exactly);
+})}
+
+
+
+
+document.addEventListener("keypress",function( event){
+     var whatspressed=event.key;
+
+     switchstatment(whatspressed);
+     Addanimation(whatspressed);
+
+
+
+})
+function switchstatment(whatspressed){
+
+     switch(whatspressed){
+
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+
+        case "W":
+            var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+
+        case "a":
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+        break;
+
+        case "A":
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+        break;
+
+        case"s":
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+
+        case"S":
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+
+        case"d":
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+        break;
+
+        case"D":
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+        break;
+
+        case"j":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+
+        case"J":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+
+
+        case"k":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        break;
+
+        case"K":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        break;
+
+
+        case"l":
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+
+        case"L":
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+
+
+
+    }
+
+
+}
+
+function Addanimation(key){
+
+    var currentkey=document.querySelector("."+ key  ) ;
+    if (currentkey){currentkey.classList.add("pressed");
+    setTimeout(function(){
+        currentkey.classList.remove("pressed");
+    },100);}
+
+}
